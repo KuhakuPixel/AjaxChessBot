281ce302a9d463a5df32e3fd4bb848c1a788d32e
KuhakuPixel <Nicholaspixels@gmail.com>
2021-07-13 14:09:04 +0700

    Get coordinates when left click

--- AjaxChessBot/AjaxChessBot.cpp
+++ AjaxChessBot/AjaxChessBot.cpp
@@ -3,13 +3,26 @@
 #include <Windows.h>
 #include "Clicker.h"
 #include <string>
+#include <vector>
+
+
+std::vector<std::string> allLeftClick = {};
+//Program main entry
 int main()
 {
-    while (true) {
-        POINT point = Clicker::GetCurrentMouseCoordinates();
-        std::cout <<"Current position of mouse: { "+std::to_string(point.x)+" , "+std::to_string(point.y)+" }"<<std::endl;
-    }
-    
+	while (true) {
+		if (GetKeyState(VK_LBUTTON) < 0) {
+			POINT point = Clicker::GetCurrentMouseCoordinates();
+			allLeftClick.push_back("{ " + std::to_string(point.x) + " , " + std::to_string(point.y) + " }");
+		
+		}
+
+
+	}
+
+
+
+
 }
 
 
