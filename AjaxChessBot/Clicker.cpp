c59711846231c21d73e30825b38605bdf748c0df
KuhakuPixel <Nicholaspixels@gmail.com>
2021-07-13 22:16:41 +0700

    Save 0

--- AjaxChessBot/Clicker.cpp
+++ AjaxChessBot/Clicker.cpp
@@ -11,20 +11,24 @@ POINT Clicker::GetCurrentMouseCoordinates() {
 void Clicker::SendLeftClick(int x, int y) {
 	
 	
-	INPUT Inputs[3] = { 0 };
+	INPUT inputs[2] = { 0 };
 	
-	Inputs[0].type = INPUT_MOUSE;
+	//Inputs[0].type = INPUT_MOUSE;
 	
-	Inputs[0].mi.dx = x; // desired X coordinate
-	Inputs[0].mi.dy = y; 
-	Inputs[0].mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+	//Inputs[0].mi.dx = x;
+	//Inputs[0].mi.dy = y; 
+	//Inputs[0].mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
 
-	Inputs[1].type = INPUT_MOUSE;
-	Inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+	inputs[0].type = INPUT_MOUSE;
+	inputs[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+	inputs[0].mi.dx = x;
+	inputs[0].mi.dy = y; 
 
-	Inputs[2].type = INPUT_MOUSE;
-	Inputs[2].mi.dwFlags = MOUSEEVENTF_LEFTUP;
-
-	SendInput(3, Inputs, sizeof(INPUT));
+	inputs[1].type = INPUT_MOUSE;
+	inputs[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+	inputs[1].mi.dx = x;
+	inputs[1].mi.dy = y;
+	SendInput(2, inputs, sizeof(INPUT));
+	
 	std::cout << "Sending left click at :"<<std::to_string(x)<<","<<std::to_string(y)<<std::endl;
 }
