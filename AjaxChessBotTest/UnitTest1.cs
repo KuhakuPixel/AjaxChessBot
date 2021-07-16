88ab0012145bf78faf0a229bfb19acf1a86c76b4
KuhakuPixel <Nicholaspixels@gmail.com>
2021-07-16 09:48:36 +0700

    Move to unit test project to Xunit because it supports parameterization testing

---
+++ AjaxChessBotTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using AjaxChessBotHelperLib;
+namespace AjaxChessBotTest
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("nononono <My Room Is Full of Bs> bla bla bla",'<', '>', false, "My Room Is Full of Bs")]
+        [InlineData("nononono \"My Room Is Full of Bs\" bla bla bla",'"', '"', false, "My Room Is Full of Bs")]
+        [InlineData("Owh no My Queen , hashTag EricROsen,",',', ',', false, " hashTag EricROsen")]
+        [InlineData("nononono <My Room Is Full of Bs> bla bla bla", '<', '>', true, "<My Room Is Full of Bs>")]
+        public void GetStringBetweenTwoCharTest(string str,char charStart,char charEnd,bool isInclusive,string expected)
+        {
+            string contentBetweenChar = AjaxStringHelper.GetStringBetweenTwoChar(str,charStart,charEnd,isInclusive);
+            Assert.Equal(expected, contentBetweenChar);
+         
+
+        }
+        
+        
+    }
+   
+}
+
